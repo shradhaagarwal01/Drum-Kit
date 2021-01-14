be871b2e0d8bdcd5de99f19310d404d6f0b91e65
shradhaagarwal01 <62115289+shradhaagarwal01@users.noreply.github.com>
2021-01-14 20:21:28 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,18 +1,20 @@
-function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
-  }
 
-  function playSound(e) {
+function playSound (e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if (!audio) return;
-
-    key.classList.add('playing');
-    audio.currentTime = 0;
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) // handle null keys
+      return;
+    audio.currentTime = 0; // allow fast play
     audio.play();
+    key.classList.add('playing');
   }
 
-const keys = Array.from(document.querySelectorAll('.key'));
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown', playSound);
+  function endSound (e) {
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!key)
+      return;
+    key.classList.remove('playing');
+  }
+
+  window.addEventListener('keydown', playSound)
+  window.addEventListener('keyup', endSound)
